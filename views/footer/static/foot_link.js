ab139f40a8b3ad78f81e0988eff7d5addf9cd30d
fangwei_mxw <test@qq.com>
2023-12-09 11:40:21 +0800

    feat: 新增标题模板1和新闻模板1

---
+++ views/footer/static/foot_link.js
@@ -0,0 +1,5 @@
+// ==================== 底部友情链接点击事件 Start ====================
+$("footer .foot-link span").on("click", function () {
+    $(".foot-online-link").slideToggle();
+});
+// ==================== 底部友情链接点击事件 End ====================
